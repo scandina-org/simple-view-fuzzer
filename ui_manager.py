59bfd4dcc973fa3808b70ab62539fcfe0e259bc1
siradheek <siradhee_k@hotmail.com>
2023-04-24 22:22:31 +0700

    enhance UI

--- ui_manager.py
+++ ui_manager.py
@@ -15,6 +15,9 @@ class UIManager:
         self.device = u2.connect()
         self.session = self.device.session(package_name, attach=True)
 
+    def reconnect_session(self):
+        self.session = self.device.session(self.package_name, attach=True)
+
     def get_page_resources(self) -> "list[Element]":
 
         # Get the layout hierarchy
@@ -50,26 +53,29 @@ class UIManager:
             self.device.press("back")
         else:
             self.device.swipe_ext(Direction.HORIZ_FORWARD)
-    def enqueue_output(self,out, queue):
+
+    def enqueue_output(self, out, queue):
         for line in iter(out.readline, b''):
             queue.put(line.decode().strip())
         out.close()
+
     def get_error_log(self):
         process_id = self.session._pidof_app(self.package_name)
-        args = ['adb','shell','logcat','*:E','-b','main,crash','--pid',str(process_id)]
-        adb_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        args = ['adb', 'shell', 'logcat', '*:E', '-b',
+                'main,crash', '--pid', str(process_id)]
+        adb_process = subprocess.Popen(
+            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
         errors = []
         q = Queue()
         t = Thread(target=self.enqueue_output, args=(adb_process.stdout, q))
-        t.daemon = True # thread dies with the program
+        t.daemon = True  # thread dies with the program
         t.start()
         while len(errors) < 10:
-            try:  
-                line = q.get(timeout=1) # or q.get(timeout=.1)
+            try:
+                line = q.get(timeout=3)  # or q.get(timeout=.1)
             except Empty:
                 break
             else:
                 errors.append(line)
         adb_process.terminate()
         return errors
-        
